d4a50350b40c360a8d0d24f543e77b1fb0855bd3
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-10-04 15:22:18 +0300

    update: route to the menu when the application start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { NavigationPaneComponent } from './core/navigation-pane/navigation-pane.component';
+import { CoffeeCardComponent } from './coffee/ui/coffee-card/coffee-card.component';
+import { DialogComponent } from './shared/dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    NavigationPaneComponent,
+    CoffeeCardComponent,
+    DialogComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "Purr' Coffee";
+
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    this.router.navigateByUrl('/menu');
+  }
+
 }
