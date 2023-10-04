0ea3e86b2dd24bd007d78b95a9bb3cce65fc459b
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-10-04 15:28:23 +0300

    update: add a custom color classes for the them

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg0: "#FFFFFF",
+        bg1: "#f1f5f9",
+        btnClr0: "#ffa16c",
+        btnClr1: "#3c3c3c",
+        txtClr0: "#ffffff",
+        txtClr1: "#3c3c3c",
+      },
+      fontFamily:{
+        Gilroy:["Gilroy",  'sans-serif']
+      }
+
+    }
   },
   plugins: [],
-}
-
+};
