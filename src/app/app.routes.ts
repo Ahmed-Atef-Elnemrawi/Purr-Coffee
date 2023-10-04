d4c62997a75a06ca4587dcce10cd41a10fc10895
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-10-04 15:27:03 +0300

    update: set the routes of the app features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { provideState } from '@ngrx/store';
+import { coffeesFeature } from './coffee/data-access/store/coffee.feature';
+import { coffeeResolver } from './coffee/utils/coffee.resolver';
+import { provideEffects } from '@ngrx/effects';
+import { CoffeeEffects } from './coffee/data-access/store/coffee.affects';
+import { orderResolver } from './order/utils/order.resolver';
+import { HistoryEffects, historyFeature } from './history/data-access/store';
+import { HistoryResolver } from './history/utils/history.resolver';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'menu',
+    loadComponent: () =>
+      import('./coffee/feature/coffee-list/coffee-list.component')
+        .then(c => c.CoffeeListComponent),
+    providers: [provideState(coffeesFeature), provideEffects(CoffeeEffects)],
+    resolve: { coffee: coffeeResolver },
+  },
+  {
+    path: 'order',
+    loadComponent:() => import('./order/feature/order/order.component')
+      .then(c => c.OrderComponent),
+    resolve: { order: orderResolver },
+  },
+  {
+    path:'history',
+    loadComponent:() => import('./history/feature/history-page/history-page.component').then(c => c.HistoryPageComponent),
+    providers:[provideState(historyFeature), provideEffects(HistoryEffects)],
+    resolve:{history:HistoryResolver}
+  }
+];
