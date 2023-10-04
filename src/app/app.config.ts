8971814efb8099b7151b6cdaa982fdf308382ce8
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-10-04 15:31:04 +0300

    update: set the preloading strategy and load the order state eagerly and provide in memory web api for the app root

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,27 @@
-import { ApplicationConfig, isDevMode } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  isDevMode,
+} from '@angular/core';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { LoggingService } from './core/logging.service';
 import { provideHttpClient } from '@angular/common/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AppDb } from './shared/DB/coffee-db';
+import { provideEffects } from '@ngrx/effects';
+import { orderFeature } from './order/data-access/store/order.feature';
+import { OrderEffects } from './order/data-access/store/order.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
     provideStore(),
+    provideState(orderFeature),
+    provideEffects([OrderEffects]),
     provideStoreDevtools({
       maxAge: 25,
       logOnly: !isDevMode(),
@@ -19,6 +30,8 @@ export const appConfig: ApplicationConfig = {
       traceLimit: 75,
     }),
     provideHttpClient(),
-    LoggingService
+    LoggingService,
+    importProvidersFrom(InMemoryWebApiModule.forRoot(AppDb, { delay: 1000 })),
+    provideEffects(),
   ],
 };
